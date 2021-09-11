0814012048ecee030ed3730f49a6ab2eedaa1925
Leshik XXL <oleksii.bikerei@gmail.com>
2021-09-11 17:38:24 +0300

    initial commit

---
+++ src/main/java/ua/leshik/financexxl/FinanceXxlApplication.java
@@ -0,0 +1,13 @@
+package ua.leshik.financexxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceXxlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceXxlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/leshik/financexxl/FinanceXxlApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.leshik.financexxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceXxlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
